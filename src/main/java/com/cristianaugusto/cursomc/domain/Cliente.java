0079f7197673adbc3efe9b1915439025309fbae7
Cristian Augusto <cristianaugusto2013@gmail.com>
2019-11-03 17:12:07 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/cristianaugusto/cursomc/domain/Cliente.java
+++ src/main/java/com/cristianaugusto/cursomc/domain/Cliente.java
@@ -26,6 +26,9 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos = new ArrayList<>();
+
     public  Cliente(){}
 
     public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -84,6 +87,10 @@ public class Cliente implements Serializable {
         return telefones;
     }
 
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -96,4 +103,6 @@ public class Cliente implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
