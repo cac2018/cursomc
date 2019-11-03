e082169259a8c4c6388d5cc754c6ab0c93098db5
Cristian Augusto <cristianaugusto2013@gmail.com>
2019-11-03 15:43:32 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/cristianaugusto/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.cristianaugusto.cursomc.services;
+
+import com.cristianaugusto.cursomc.domain.Cliente;
+import com.cristianaugusto.cursomc.repositories.ClienteRepository;
+import com.cristianaugusto.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id
+                + ", Tipo: " + Cliente.class.getName()));
+    }
+
+}
