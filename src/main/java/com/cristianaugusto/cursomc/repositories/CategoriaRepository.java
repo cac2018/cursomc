0fcecc841b3b479ed0cbc6373d7f550d1de0bb8d
Cristian Augusto <cristianaugusto2013@gmail.com>
2019-11-03 12:24:20 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/cristianaugusto/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.cristianaugusto.cursomc.repositories;
+
+import com.cristianaugusto.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
